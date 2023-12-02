d235a193866b04d6834f6c81c71a0c4b587d8f39
Steven Atkinson <mrstebouk@gmail.com>
2023-12-02 16:54:15 +0000

    Added day 1

---
+++ day_1/app.rb
@@ -0,0 +1,7 @@
+result = File.open("input.txt").readlines.map(&:chomp).inject(0) do |sum, line|
+    numbers = line.split("").map(&:to_i).filter { |n| n > 0 }
+    sum += "#{numbers.first}#{numbers.last}".to_i
+end
+
+# Print the sum of all the sums
+puts result
