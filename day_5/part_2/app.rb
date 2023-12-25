1c2a95eef6abebac59d864c3a9fa32b042e0255c
Steven Atkinson <mrstebouk@gmail.com>
2023-12-25 09:10:00 +0000

    Creating each map in a hash using "to" for the sequence

--- day_5/part_2/app.rb
+++ day_5/part_2/app.rb
@@ -22,75 +22,12 @@ end
 
 class Almanac
     def initialize
-        @data = File.read('../input.txt')
+        @data = File.read('../test.txt')
     end
 
     def seeds
-        valid_seeds = seed_ranges.map do |start, finish|
-            seed_to_soil_map.map do |map|
-                # Pin the start range to the first valid source range on the map
-                pinned_start = map.source_range[0] if start < map.source_range[0]
-
-                # Pin the finish range to the last valid source range on the map
-                pinned_finish = map.source_range[1] if finish > map.source_range[1]
-
-                # If the start and finish ranges overlap, then we have a valid seed
-                # p (start..finish)
-                [pinned_start || start, pinned_finish || finish]
-            end
-        end
-
-        p valid_seeds.flatten.uniq
+        @data.split("\n")[0].split(' ').map(&:to_i).select { |n| n > 0 }
     end
-
-    def each_seed(&block)
-        seed_ranges.each do |start, finish|
-            seed_to_soil_map.each do |map| 
-                (map.destination_value_for(start)..map.destination_value_for(finish)).each do |seed|
-                    block.call(seed)
-                end
-            end
-         end
-    end
-
-    def get_locations_for(seed)
-        soils = get_mappings_for(seed, seed_to_soil_map)
-        return soils if soils.empty?
-
-        fertilizers = soils
-            .map { |soil| get_mappings_for(soil, seed_to_fertilizer_map) }
-            .flatten
-        return fertilizers if fertilizers.empty?
-
-        waters = fertilizers
-            .map { |fertilizer| get_mappings_for(fertilizer, fertilizer_to_water_map) }
-            .flatten
-        return waters if waters.empty?
-
-        lights = waters
-            .map { |water| get_mappings_for(water, water_to_light_map) }
-            .flatten
-        return lights if lights.empty?
-
-        temperatures = lights
-            .map { |light| get_mappings_for(light, light_to_temperature_map) }
-            .flatten
-        return temperatures if temperatures.empty?
-
-        humidities = temperatures
-            .map { |temperature| get_mappings_for(temperature, temperature_to_humidity_map) }
-            .flatten
-        return humidities if humidities.empty?
-
-        locations = humidities
-            .map { |humidity| get_mappings_for(humidity, humidity_to_location_map) }
-            .flatten
-        return locations if locations.empty?
-
-        locations
-    end
-
-    private
 
     def seed_ranges
         @data
@@ -100,89 +37,59 @@ class Almanac
             .select { |n| n > 0 }
             .each_slice(2)
             .map { |start, length| [start, start + length - 1] }
+            .sort { |a, b| b[0] <=> a[0] }
     end
 
-    def seed_to_soil_map
-        unless @seed_to_soil_map
-            @seed_to_soil_map = get_map_rows_for('seed-to-soil')
+    def walk(value, range, name = nil)
+        if name.nil?
+            return walk(value, range, create_maps.keys.first)
         end
-        @seed_to_soil_map
-    end
+        
+        item = create_maps[name]
 
-    def seed_to_fertilizer_map
-        unless @seed_to_fertilizer_map
-            @seed_to_fertilizer_map = get_map_rows_for('soil-to-fertilizer')
-        end
-        @seed_to_fertilizer_map
-    end
-
-    def fertilizer_to_water_map
-        unless @fertilizer_to_water_map
-            @fertilizer_to_water_map = get_map_rows_for('fertilizer-to-water')
-        end
-        @fertilizer_to_water_map
-    end
-
-    def water_to_light_map
-        unless @water_to_light_map
-            @water_to_light_map = get_map_rows_for('water-to-light')
-        end
-        @water_to_light_map
-    end
-
-    def light_to_temperature_map
-        unless @light_to_temperature_map
-            @light_to_temperature_map = get_map_rows_for('light-to-temperature')
-        end
-        @light_to_temperature_map
-    end
-
-    def temperature_to_humidity_map
-        unless @temperature_to_humidity_map
-            @temperature_to_humidity_map = get_map_rows_for('temperature-to-humidity')
-        end
-        @temperature_to_humidity_map
-    end
-
-    def humidity_to_location_map
-        unless @humidity_to_location_map
-            @humidity_to_location_map = get_map_rows_for('humidity-to-location')
-        end
-        @humidity_to_location_map
-    end
-
-    def get_map_rows_for(name)
-        rows = @data.split("\n")
-        index = rows.index { |row| row.include?(name) }
-        rows = rows[index+1..-1]
-        last_index = rows.index { |row| row.empty? } || rows.length
-        rows[0..last_index-1]
-            .map { |row| row.split(' ').map(&:to_i) }
-            .map { |row| DestinationCategory.new(*row) }
-    end
-
-    def get_mappings_for(seed, mapping)
-        results = mapping
-            .map { |map| map.destination_value_for(seed) }
-            .uniq
-
-        # If the results contain anything other than the seed value, then we need to remove the seed value
-        # from the results.
-        if results.length > 1 && results.include?(seed)
-            results.delete(seed)
+        if item.nil?
+            puts "Found #{value} in #{name}"
+            return
         end
 
-        results
+        range_item = item[:map].find { |i| i.source_range[0] <= value && i.source_range[1] >= value }
+
+        if range_item.nil?
+            puts "Could not find #{value} in #{name}"
+            return walk(value, 1, item[:to])
+        end
+
+        puts "Found #{value} in #{name} (#{range_item.source_range} -> #{range_item.destination_range})"
+        walk(range_item.destination_value_for(value), range, item[:to])
+    end
+
+    private
+
+    def create_maps
+        unless @maps
+            @maps = Hash.new
+
+            @data.split("\n").each do |line|
+                if line.empty?
+                    next
+                elsif line.include?('to')
+                    from, to = line.match(/(\w+)-to-(\w+)/).captures
+                    @maps[from] = { to: to, map: [] }
+                elsif @maps.keys.length > 0
+                    @maps[@maps.keys.last][:map] << DestinationCategory.new(*line.split(' ').map(&:to_i))
+                end
+            end
+        end
+        @maps
     end
 end
 
 almanac = Almanac.new
 
-lowest_location = nil
-almanac.seeds.each do |seed|
-# almanac.each_seed do |seed|
-    locations = almanac.get_locations_for(seed)
-    lowest_location = locations.min if lowest_location.nil? || locations.min < lowest_location
-end
+# almanac.seed_ranges.each do |range|
+#     almanac.walk(range[0], range)
+# end
 
-puts lowest_location
+almanac.seeds.each do |seed|
+    almanac.walk(seed, 1)
+end
